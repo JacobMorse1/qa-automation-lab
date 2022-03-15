d0c13ba3feb16e077b9e282fad15a7e9b4790b5d
Jacob Morse <jacobsmorse@gmail.com>
2022-03-15 16:49:15 -0600

    added another test

--- movieList/functions/deleteMovie.js
+++
@@ -1,9 +0,0 @@
-const {By} = require('selenium-webdriver')
-
-const deleteMovie = async (driver) => {
-    await driver.findElement(By.xpath('//button[text()= "x"')).click()
-}
-
-module.exports = {
-    deleteMovie
-}    
--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -1,16 +1,23 @@
+// BOILERPLATE for SELENIUM
+
 const {Builder, Capabilities, By} = require('selenium-webdriver') 
 
 require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
+// CONNECTS TO WEBPAGE THAT WILL BE TESTED
 beforeAll(async () => {
     await driver.get('http://127.0.0.1:5501/movieList/index.html')
 })
 
+// CLOSES WEBPAGE THAT WILL BE TESTED
 afterAll(async () => {
     await driver.quit()
 })
+
+
+//FUNCTIONS
 
 const crossOutMovie = async driver => {
     await driver.findElement(By.xpath('//span')).click()
@@ -33,10 +40,24 @@ const addMovie = async (driver) => {
     expect(movie.isDisplayed()).toBeTruthy()
 }
 
+const checkAside = async driver => {
+    let message = await driver.findElement(By.css('#message'))
+
+    expect(message.isDisplayed()).toBeTruthy()
+}
+
+
+// ---TESTS--- that call the functions
+
 test('adds movie to list', async () => {
     await addMovie(driver)
 
     await driver.sleep(2000)
+})
+
+test('verify that message appears when movie is added', async () => {
+    await checkAside(driver)
+
 })
 
 test('crosses out movie from list', async () => {
